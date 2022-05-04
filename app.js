4746c3af56b6bd9e04b7644c7f460fffe4fb025e
HCarson2 <mair0610@gmail.com>
2022-05-03 23:25:59 -0400

    later

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-let imgOver = document.querySelector('#imgOver').classList.remove("hidden")
+let gameOver = document.querySelector('#gameOver')
+let body = document.querySelector('body')
+if (score === 10){
+    gameOver.classList.remove('hidden')
+}
