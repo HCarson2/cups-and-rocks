f22e4b66b08926c9affb04067d7d1ec37daafe25
HCarson2 <mair0610@gmail.com>
2022-05-04 16:04:27 -0400

    us

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 //-----------------------------------------------------
-         Variables
+        //  Variables
 //-----------------------------------------------------
-var scoreboardNameA;
-var scoreboardNameB;
-var scoreboardNamePointA;
-var scoreboardNamePointb;
-var youWon;
-var hidden;
-var wrong;
-var right;
+let playerOne;
+let playerTwo;
+let scoreboardPointsA;
+let scoreboardPointsB;
+let youWon;
+let hidden;
+let wrong;
+let right;
+let playerOneJustScored;
+let playerTwoJustScored;
+
+
+//-------------------------------------------------------
+
+playerOne = 0
+
+    playerOneJustScored = () =>{
+    scoreboardPointsA.score++
+    let screen = document.querySelector('.scoreboard')
+    screen.textContent = `score : ${scoreboardPointsA.score}`
+
+// } 
+//  playerOneJustScored = document.querySelector('scoreboard')
+
+
+
 
 
 // let youWon = document.querySelector('#youWon')
