c72608c85e6d9514302fcaf11a3afbbc0a70a183
HCarson2 <mair0610@gmail.com>
2022-05-05 20:19:05 -0400

    food

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 //-----------------------------------------------------
         //  Variables
 //-----------------------------------------------------
-let scory = {
-    scoreboardPointsA: 0,
-    scoreboardPointsB: 0,
+let playerOne = {
+    myTurn: false,
+    won_it_all: false,
+    score: 0,
+}
+let playerTwo = {
+    myTurn: false,
+    won_it_all: false,
+    score: 0,
+
+}
+const determineRound = () =>{
+    if (playerOne.score >= 3 || playerTwo.score >= 3){
+        alert ("You won Round 1")
+        alert ("Begin Round 2")
+    }
+    if (playerOne.score >= 6 || playerTwo.score >= 6){
+        alert ("You Won Round 2")
+        alert ("Begin Round 3")
+    }
+    if (playerOne.score >= 9 || playerTwo.score >= 9){
+        alert ("You Won Round 3")
+        alert ("You Are The Winner!")
+    }
+}
+
+const addScoreToPlayerOne = () =>{
+
+
+}
+const addScoreToPlayerTwo = () =>{
+
+}
+    // scoreboardPointsB: 0,
 
     // scoreboardPointsA: 0
     // scoreboardPointsB: 0
@@ -21,43 +52,15 @@ let scory = {
 
 //-------------------------------------------------------
 
-// scoreboardPointsA = 0
-// scoreboardPointsB = 0
-// let button = document.getElementById("a");
-// button.addEventListener("click", function(){
-// });
+
 
 const playA = () =>{
-    scory.scoreboardPointsA++
-    let screen = document.querySelector('.scoreboardPointsA')
-    screen.textContent= `${scory.scoreboardPointsA}`
-    
-    if ('scoreboardPointsA' === 10){
-        imgWinner.classList.remove('hidden')
-    }
+
 }
-// const b = () =>{
-//     scory.scoreboardPointsB++
-//     let scan = document.querySelector('.scoreboardPointsB')
-//     screen.textContent= `${scory.scoreboardPointsB}`
-
-
-// playerTwoJustScored = scoreboardPointsB++;
-// console.log('scoreboardPointsA');
-
-
-    // playerOneJustScored = () =>{
-    // scoreboardPointsA.score++
-    // let screen = document.querySelector('.scoreboard')
-    // screen.textContent = `score : ${scoreboardPointsA.score}`
-
-// } 
-//  playerOneJustScored = document.querySelector('scoreboard')
-
-
-
-
-// let youWon = document.querySelector('#youWon')
-// let body = document.querySelector('body')
-
+    
+    
+    
+    // score.scoreboardPointsA++
+    // let screen = document.querySelector('.scoreboardPointsA')
+    // screen.textContent= `${score.scoreboardPointsA}`
 
