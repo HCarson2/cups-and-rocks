c02893437a9dadfe7abd3cec165b3882f71c46c9
HCarson2 <mair0610@gmail.com>
2022-05-06 11:11:13 -0400

    yeah

--- app.js
+++ app.js
@@ -12,6 +12,54 @@ let playerTwo = {
     score: 0,
 
 }
+
+const wait = (ms) => new Promise (resolve => setTimeout(resolve, ms))
+const btnStart = document.getElementById('btn-start')
+let cup1 = document.querySelector('.cup1')
+let cup2 = document.querySelector('.cup2')
+let cup3 = document.querySelector('.cup3')
+
+const animations = async () =>{
+   cup1.classList.add('cupOne')
+   cup2.classList.add('cupTwo')
+   cup3.classList.add('cupThree')
+   await wait(3000)
+   cup1.classList.remove('cupOne')
+   cup2.classList.remove('cupTwo')
+   cup3.classList.remove('cupThree')
+
+}
+const reveal = () =>{
+    // console.log('abc')
+  let randomNum =  Math.floor(Math.random() * 3);
+//   console.log(randomNum)
+if (randomNum ===0){
+    console.log('winner')
+}else if (randomNum === 1){
+    console.log('you lose')
+}else if(randomNum=== 2){
+    console.log('almost')
+}else{
+    console.log('error')
+}
+cup1.removeEventListener('click',reveal)
+cup2.removeEventListener('click',reveal)
+cup3.removeEventListener('click',reveal)
+
+}   
+
+cup1.addEventListener('click',reveal)
+cup2.addEventListener('click',reveal)
+cup3.addEventListener('click',reveal)
+
+// function startGame(){
+
+
+btnStart.addEventListener('click', animations)
+// btnStart.addEventListener('click', bounceup)
+// btnStart.addEventListener('click', leftroll)
+
+
 const determineRound = () =>{
     if (playerOne.score >= 3 || playerTwo.score >= 3){
         alert ("You won Round 1")
@@ -34,11 +82,22 @@ const addScoreToPlayerOne = () =>{
 const addScoreToPlayerTwo = () =>{
 
 }
+const playA = () =>{
+    playerOne.score++
+    let screen = document.querySelector('.scoreboardPointsA')
+    screen.textContent= `${playerOne.score}`    
+} 
+const playB = () =>{
+    playerTwo.score++
+    let screen = document.querySelector('.scoreboardPointsB')
+    screen.textContent= `${playerTwo.score}`
+}
+
     // scoreboardPointsB: 0,
 
     // scoreboardPointsA: 0
     // scoreboardPointsB: 0
-}
+
 // let playerOne;
 // let playerTwo;
 
@@ -54,13 +113,11 @@ const addScoreToPlayerTwo = () =>{
 
 
 
-const playA = () =>{
 
-}
+
+
     
     
     
-    // score.scoreboardPointsA++
-    // let screen = document.querySelector('.scoreboardPointsA')
-    // screen.textContent= `${score.scoreboardPointsA}`
+    
 
